0179f1b94ba110eebbfeaf2939f40d1795c1c616
Eduard-Bodreev <edik-bodreev@mail.ru>
2025-02-16 20:37:00 +0300

    fix run.sh for CI/CD

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,11 +3,16 @@
 set -e
 
 echo "Проверяем, установлен ли Go..."
-if ! command -v go &> /dev/null
-then
+if ! command -v go &> /dev/null; then
     echo "Go не найден! Установите Go и повторите попытку."
     exit 1
 fi
 
 echo "Запускаем сервер..."
-go run ./cmd/main.go
+go run ./cmd/main.go &
+SERVER_PID=$!
+
+echo "Ждем, что сервер запустился..."
+sleep 5
+
+echo "Сервер запущен с PID $SERVER_PID"
